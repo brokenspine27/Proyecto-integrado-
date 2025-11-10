b62f429e28d20995375779ebb0d5a42c2870be72
brokenspine27 <doris.ilabaca@inacapmail.cl>
2025-11-10 19:43:59 -0300

    mejora de comentarios

--- calificaciones/ingresar_montos.py
+++ calificaciones/ingresar_montos.py
@@ -1,4 +1,3 @@
-# calificaciones/urls.py
 from django.urls import path
 from . import views
 
@@ -6,10 +5,10 @@ urlpatterns = [
     path('', views.mantenedor_calificaciones, name='mantenedor'),
     path('ingresar/', views.ingresar_calificacion, name='ingresar_calificacion'),
     
-    # --- NUEVA RUTA para el formulario de montos (Paso 2) ---
-    # Usamos <int:calificacion_id> para saber a qué registro añadirle los montos
+    #ruta del formulario de montos
     path('ingresar-montos/<int:calificacion_id>/', views.ingresar_montos, name='ingresar_montos'),
-    
+    #ruta de carga masiva con factores
     path('upload/factores/', views.carga_masiva_factores, name='carga_masiva_factores'),
+    #ruta de carga masiva con montos
     path('upload/montos/', views.carga_masiva_montos, name='carga_masiva_montos'),
 ]
